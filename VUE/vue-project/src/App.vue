48a089d028c8c0009890588fd53f0415e4d3d7fc
유준호 <juno980220@naver.com>
2025-03-11 09:18:43 +0900

    web

---
+++ VUE/vue-project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
